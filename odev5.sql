f14fe0a248ced4f851dab975922534b692b7c428
Uğur Hüseynəliyev <141139244+ugurhuseynaliyev@users.noreply.github.com>
2025-10-06 19:54:01 +0400

    Patika.dev SQL odev 5

--- odev5.sql
+++ odev5.sql
@@ -5,4 +5,5 @@ Select * from film Where title like '%n' Order by length Desc limit 5
 Select * from film Where title like '%n' Order by length ASC offset 5 limit 5
 
 -- 3. customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 olmak koşuluyla ilk 4 veriyi sıralayınız.
+
 Select * from customer Where store_id = 1 Order By last_name DESC limit 4
